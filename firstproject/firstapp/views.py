550f0b2623d4b2a7a73502de592070d61b8479f2
Tanisshh <tanjilislam288@gmail.com>
2021-12-30 00:19:15 +0600

    pushing data on templates

--- firstproject/firstapp/views.py
+++ firstproject/firstapp/views.py
@@ -6,7 +6,7 @@ from firstapp.models import Record, Topic, WebPage
 def index(request):
     date_list = Record.objects.order_by('date')
     webpage_dict = {'accessrecords':date_list}
-    return render(request, 'index.html', context=webpage_dict)
+    return render(request, 'index.html', context = webpage_dict)
 
 def index2(request):
     return HttpResponse("hello im from views.py file")
