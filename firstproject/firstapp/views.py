21119c2d8e59a7877e3fb5807370a407395c93bc
Tanisshh <tanjilislam288@gmail.com>
2021-12-30 00:18:05 +0600

    pushing data on templates

--- firstproject/firstapp/views.py
+++ firstproject/firstapp/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from firstapp.models import Record, Topic, WebPage
 
 
 def index(request):
-    return render(request, 'index.html')
+    date_list = Record.objects.order_by('date')
+    webpage_dict = {'accessrecords':date_list}
+    return render(request, 'index.html', context=webpage_dict)
 
 def index2(request):
     return HttpResponse("hello im from views.py file")
